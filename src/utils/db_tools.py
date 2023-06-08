e222af0780e04b7ce32698759bbb99f18755639c
CassiaFa <fab.c1548+GitHub@gmail.com>
2023-06-08 17:45:31 +0200

    Update docstring of db_tool

--- src/utils/db_tools.py
+++ src/utils/db_tools.py
@@ -36,6 +36,11 @@ class Database():
 
     @classmethod
     def open_connexion(cls):
+        """
+        This class method opens a connection to a MySQL database and returns an instance of a cursor that can be used to 
+        execute SQL commands. It takes no parameters. It connects to the database using the class variables __USER, 
+        __PWD, __HOST, __PORT, and __DB. If the cursor is already initialized, it does not try to connect again.
+        """
         if not cls.__cursor:
             cls.__bdd = mysqlpy.connect(
                                         user = cls.__USER,
@@ -49,12 +54,32 @@ class Database():
     
     @classmethod
     def close_connexion(cls):
+        """
+        Closes the connection to the database and sets the cursor object to None.
+
+        :param cls: the class object.
+        :return: None
+        """
         cls.__cursor.close()
         cls.__bdd.close()
         cls.__cursor = None
     
     @classmethod
     def add_user(cls, gender, first_name, last_name, username, email, password):
+        """
+        Adds a new user to the database with the specified gender, first name, last name,
+        username, email, and password. The password is encrypted using the secure encryption
+        provided by the class. The function does not return anything.
+
+        Args:
+            gender (str): A string representing the gender of the user.
+            first_name (str): A string representing the first name of the user.
+            last_name (str): A string representing the last name of the user.
+            username (str): A string representing the username of the user.
+            email (str): A string representing the email of the user.
+            password (str): A string representing the password of the user.
+        """
+
         password = cls.__secure.encrypt(password, cls.__cursor)
         querry = "INSERT INTO users (gender, first_name, last_name, username, email, password) VALUES (%s, %s, %s, %s, %s, %s);"
         param = (gender, first_name, last_name, username, email, password)
@@ -63,6 +88,17 @@ class Database():
 
     @classmethod
     def check_user(cls, username, password):
+        """
+        Check if the provided username and password match a user in the database.
+
+        Args:
+            username (str): The username of the user to check.
+            password (str): The password of the user to check.
+
+        Returns:
+            dict: The user dictionary if the username and password match, False otherwise.
+        """
+
         querry = f"SELECT * FROM users WHERE username='{username}';"
         cls.__cursor.execute(querry)
 
@@ -76,6 +112,19 @@ class Database():
     
     @classmethod
     def get_files(cls, id_file=None, time_min=None, time_max=None, name=None):
+        """
+        A class method that retrieves files from the FILES table in the database.
+
+        Args:
+            id_file (int, optional): The id of the file to retrieve.
+            time_min (str, optional): The minimum date of files to retrieve.
+            time_max (str, optional): The maximum date of files to retrieve.
+            name (str, optional): The name of the file to retrieve.
+
+        Returns:
+            list[dict]: A list of dict if there are multiple, or a dict if there is only one.
+        """
+
 
         if id_file:
             if time_min and time_max:
@@ -118,6 +167,17 @@ class Database():
     
     @classmethod
     def get_detections(cls, id_file=None, time_min=None, time_max=None):
+        """
+        A class method that retrieves detections from the DETECTIONS table in the database.
+
+        Args:
+            id_file (int, optional): An optional integer representing the id of the file to filter the detections by.
+            time_min (str, optional): An optional string representing the minimum time to filter the detections by.
+            time_max (str, optional): An optional string representing the maximum time to filter the detections by.
+
+        Returns:
+            list[dict]: A list of dict containing the start and stop times of each detection, and the id of the detected species.
+        """
 
         if id_file:
             if time_min and time_max:
@@ -139,6 +199,20 @@ class Database():
     
     @classmethod
     def get_projects(cls, name=None, depth=None, lat=None, long=None):
+        """
+        Retrieves projects from the database based on optional filters.
+
+        Args:
+            name (str, optional): String representing the name of the project to retrieve.
+            depth (int, optional): Integer representing the depth of the project to retrieve.
+            lat (float, optional): Float representing the latitude of the project to retrieve.
+            long (float, optional): Float representing the longitude of the project to retrieve.
+
+        Returns:
+            list[dict]: A list of dictionaries representing the retrieved projects, or a dictionary representing a 
+            single project if only one is retrieved.
+        """
+
         if name:
             querry = f"SELECT * FROM PROJECTS WHERE name='{name}';"
         elif depth:
@@ -162,6 +236,19 @@ class Database():
 
     @classmethod
     def add_project(cls, name, depth, lat, long):
+        """
+        Inserts a new project in the PROJECTS table with the given name, depth, latitude, and longitude.
+
+        Args:
+            name (str): The name of the project to insert.
+            depth (float): The depth of the project to insert.
+            lat (float): The latitude of the project to insert.
+            long (float): The longitude of the project to insert.
+
+        Returns:
+            None
+        """
+        
         querry = "INSERT INTO PROJECTS (name, depth, latitude, longitude) VALUES (%s, %s, %s, %s);"
         param = (name, depth, lat, long)
         cls.__cursor.execute(querry, param)
@@ -169,6 +256,12 @@ class Database():
 
     @classmethod
     def check_database(cls):
+        """
+        This is a class method that checks if there are tables in the database and if they are empty.
+        
+        Returns:
+            bool : True if there are tables in the database and they are not empty, otherwise it prints an error message.
+        """
         querry = "SHOW TABLES;"
         cls.__cursor.execute(querry)
         
@@ -209,6 +302,22 @@ class Database():
 
     @classmethod
     def add_file(cls, name, date, duration, fs, path, id_project):
+        """
+        Inserts a new file into the FILES table given its name, date, duration, fs, path,
+        and id_project.
+
+        Args:
+            name (str): The name of the file.
+            date (str): The date when the file was created.
+            duration (int): The duration of the file in seconds.
+            fs (str): The file system format of the file.
+            path (str): The path of the file.
+            id_project (int): The id of the project that the file belongs to.
+
+        Returns:
+            None
+        """
+
         querry = "INSERT INTO FILES (name, date, duration, fs, path, id_project) VALUES (%s, %s, %s, %s, %s, %s);"
         param = (name, date, duration, fs, path, id_project)
         cls.__cursor.execute(querry, param)
@@ -217,6 +326,20 @@ class Database():
 
     @classmethod
     def add_detection(cls, start, stop, confidence, id_species, id_file):
+        """
+        Adds a new detection to the database.
+
+        Args:
+            start (int): Start time of the detection.
+            stop (int): Stop time of the detection.
+            confidence (float): Confidence level of the detection.
+            id_species (int): ID of the species detected.
+            id_file (int): ID of the file containing the detection.
+
+        Returns:
+            None
+        """
+
         querry = "INSERT INTO DETECTIONS (start, stop, confidence ,id_file, id_species) VALUES (%s, %s, %s, %s, %s);"
         param = (start, stop, float(confidence), id_file, id_species)
         cls.__cursor.execute(querry, param)        
